d45e92460f4833056302d1d1f67cdb75cf298a40
Justen-N <losthalflingrogue@gmail.com>
2019-09-05 22:08:33 -0600

    Brought up to date, follow LDT standards.

---
+++ src/api/java/com/minecolonies/api/compatibility/animania/AnimaniaCompat.java
@@ -0,0 +1,9 @@
+package com.minecolonies.api.compatibility.animania;
+
+public class AnimaniaCompat {
+
+    protected boolean isAnimaniaPresent(){
+        return true;
+    }
+
+}
---
+++ src/api/java/com/minecolonies/api/compatibility/animania/AnimaniaProxy.java
@@ -0,0 +1,14 @@
+package com.minecolonies.api.compatibility.animania;
+
+public class AnimaniaProxy {
+
+    protected  boolean isAnimaniaPresent()
+    {
+        return false;
+    }
+    protected boolean isAnimalAgendered()
+    {
+        return false;
+    }
+
+}
